5d4299305e76f096600cb9b2d1b297af107ea1dc
Jacob Repp <jacobrepp@gmail.com>
2017-11-30 05:30:38 -0800

    Renaming projects

--- build_test.sh
+++ build_test.sh
@@ -10,9 +10,8 @@ pushd build
 make all
 
 # Run the tests collecting failures
-bin/test_app > test_results.md
+bin/ipv6-test > test_results.md
 failures=`grep -c FAIL test_results.md`
 popd
 
 echo "Completed test pass, ${failures} failures. See build/test_results.md"
-
