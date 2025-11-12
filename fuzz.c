6e828d446961cff3f317b2c033b77784e7b27432
Jacob Repp <jacobrepp@gmail.com>
2025-11-12 13:46:31 -0800

    Fix Windows SDK warnings by defining WIN32_LEAN_AND_MEAN and _WIN32_WINNT

--- fuzz.c
+++ fuzz.c
@@ -28,6 +28,12 @@
 #include <time.h>
 
 #ifdef _WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+        #define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef _WIN32_WINNT
+        #define _WIN32_WINNT 0x0600  // Windows Vista or later
+    #endif
     #include <windows.h>
 #else
     #include <sys/time.h>
